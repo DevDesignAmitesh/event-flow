e8e33b3c9083e1b89317b668b332391cc8e3d4b6
Amitesh Singh <amiteshsingh252@gmail.com>
2025-04-27 09:17:10 +0530

    same site none

--- http-backend/routes/auth/index.ts
+++ http-backend/routes/auth/index.ts
@@ -40,7 +40,7 @@ authRouter.post(
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-        sameSite: "lax",
+        sameSite: "none",
       });
 
       res.status(201).json({
@@ -77,7 +77,7 @@ authRouter.post(
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
         maxAge: 7 * 24 * 60 * 60 * 1000,
-        sameSite: "lax",
+        sameSite: "none",
       });
 
       res.status(200).json({
